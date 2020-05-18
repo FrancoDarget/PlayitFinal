b6f4748f5e599b6e150b6cd29f1b992f104352e4
Franco <dargetfranco@gmail.com>
2020-05-18 17:26:23 -0300

    Base de datoss

--- controller/controlador.js
+++ controller/controlador.js
@@ -1,6 +1,5 @@
-const playitBD = require('basededatos'); // Agarra la base de datos
+const playitBD = require('playitBD'); // Agarra la base de datos
 const bcrypt = require('bcryptjs'); // Comando para incriptar la data
-let passEncriptada = bcrypt.hashSync(req.body.formlogin.password, 10); // Incripta los datos para usar en la pass
 
 // Controlador: Objeto literal que tiene todos los controladores de la ruta. 
 let controlador = {
@@ -34,13 +33,14 @@ let controlador = {
            res.send ("HAY ERRORES") // Si hay errores en los datos completados por el usuario, salta un cartel de ERROR
        } 
        else{ // Estoy creando un nuevo usuario en la base de datos con la informacion que el usuario completo en el registration. 
+        let passEncriptada = bcrypt.hashSync(req.body.password, 10); // Incripta los datos para usar en la pass
            playitBD.usuarios.create({
-               name: req.body.formlogin.name, // Esto toma el name que el usuario completo en el form de registration
-               email: req.body.formlogin.email,
+               name: req.body.user, // Esto toma el name que el usuario completo en el form de registration
+               email: req.body.email,
                password:passEncriptada, // Guarda la password encriptada.
-               birthdate: req.body.formlogin.date,
+               birthdate: req.body.date,
            }) 
-       
+       res.render('home');
         }
     }  
 }
